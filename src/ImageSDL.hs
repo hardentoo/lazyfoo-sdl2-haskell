aabd479249ce856dc07467d242e7ecb1902693be
Daniel Taskoff <daniel.taskoff@gmail.com>
2015-12-09 14:15:39 +0200

    Rewrite second lesson to use functions from Core

--- src/ImageSDL.hs
+++ src/ImageSDL.hs
@@ -1,31 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import SDL
-import Linear
-import Prelude hiding (init)
+import Data.Array
+import Control.Monad.State
+import Control.Arrow
+import Core
 
-
-init :: IO Window
-init = do
-  initialize [InitVideo]
-
-  createWindow "Image SDL" $
-    defaultWindow { windowInitialSize = V2 600 480 }
-
-loadMedia :: Window -> IO ()
-loadMedia w = do
-  windowSurface <- getWindowSurface w
-  imageSurface <- loadBMP "./data/kitty.bmp"
-  surfaceBlit imageSurface Nothing windowSurface Nothing
-  updateWindowSurface w
-
-close :: IO ()
-close = do
-  delay 2000
-  quit
 
 main :: IO ()
 main = do
-  init >>=
-    loadMedia
-  close
+  s <- execStateT initState undefined
+  s <- execStateT (loadMedia [(DefaultS, "./data/kitty.bmp")]) s
+  s <- execStateT (reloadWindowSurface DefaultS) s
+  delay 2000
+  evalStateT close s
