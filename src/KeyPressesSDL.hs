5430ed4befa2bf36aff777b06b11f6f374b18d61
Daniel Taskoff <daniel.taskoff@gmail.com>
2015-12-09 14:18:26 +0200

    Rewrite fourth lesson to use functions from Core
    FRP.Yampa is an overkill yet, so stick with StateT and manual loops.

--- src/KeyPressesSDL.hs
+++ src/KeyPressesSDL.hs
@@ -2,38 +2,20 @@
 
 import SDL
 import Linear
-import Prelude hiding (init)
-import FRP.Yampa (reactimate, arr)
-import Data.IORef
+import Data.Array
+import Control.Monad
+import qualified Control.Monad.State as ST
+import Core
+import Control.Arrow
 
-
-init :: IO Window
-init = do
-  initialize [InitVideo]
-
-  createWindow "Image SDL" $
-    defaultWindow { windowInitialSize = V2 600 480 }
-
-loadMedia :: IO [Surface]
-loadMedia = mapM loadBMP
-  ["./data/kitty.bmp", "./data/kitty-up.bmp", "./data/kitty-down.bmp"]
-
-reloadMedia :: Window -> Surface -> Surface -> IO ()
-reloadMedia w windowSurface imageSurface = do
-  surfaceBlit imageSurface Nothing windowSurface Nothing
-  updateWindowSurface w
-
-close :: IO ()
-close = do
-  quit
 
 data Action = UpKey | DownKey | Quit | Other deriving (Eq)
 
-getAction :: Event -> Action
-getAction (Event {eventPayload = QuitEvent}) = Quit
-getAction (Event {eventPayload = KeyboardEvent KeyboardEventData
+getAction :: EventPayload -> Action
+getAction QuitEvent = Quit
+getAction (KeyboardEvent KeyboardEventData
                   { keyboardEventKeyMotion = Pressed
-                  , keyboardEventKeysym = Keysym k _ _}}) =
+                  , keyboardEventKeysym = Keysym k _ _}) =
   case k of
   ScancodeUp   -> UpKey
   ScancodeDown -> DownKey
@@ -43,25 +25,23 @@ getAction _ = Other
 
 main :: IO ()
 main = do
-  window <- init
-  windowSurface <- getWindowSurface window
-  [defaultSurface, upSurface, downSurface] <- loadMedia
+  s <- ST.execStateT initState undefined
+  s <- ST.execStateT (loadMedia [ (DefaultS, "./data/kitty.bmp")
+                                , (ImageUpS, "./data/kitty-up.bmp")
+                                , (ImageDnS, "./data/kitty-down.bmp")
+                                ]) s
 
-  t <- time
-  timeRef <- newIORef t
+  let loop = do
+        event <- fmap eventPayload waitEvent
+        ST.evalStateT (reloadWindowSurface
+          (case getAction event of
+            UpKey   -> ImageUpS
+            DownKey -> ImageDnS
+            _       -> DefaultS
+          )) s
 
-  let sense   = (\_ -> do
-        t' <- readIORef timeRef
-        let dt = t' - t
-        writeIORef timeRef dt
-        pollEvent >>= return . (,) dt)
-      actuate = (\_ action -> reloadMedia window windowSurface
-                                (case action of
-                                  UpKey   -> upSurface
-                                  DownKey -> downSurface
-                                  _       -> defaultSurface)
-                            >> return (action == Quit))
-      sf      = arr getAction
+        unless (event == QuitEvent) loop
 
-  reactimate waitEvent sense actuate sf
-  close
+  loop
+
+  ST.evalStateT close s
